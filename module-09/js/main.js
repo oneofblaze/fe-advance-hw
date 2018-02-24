7eea4e9e515a209f1b922c9aa7f6d51a45e6fc24
Pavlo Bezsmertnyy <32062710+oneofblaze@users.noreply.github.com>
2018-02-24 19:03:20 +0200

    module-9 update

--- module-09/js/main.js
+++ module-09/js/main.js
@@ -1,39 +1,45 @@
 /*
   Написать скрипт который собирает 3 строки клавиатуры и клавишу "пробел" из шаблона по заданому объекту.
-  Для зарендереной клавиатуры реализовать поведение из модуля 8, подсветка нажатой клавиши, отображение символа итд.
+  Для зарендереной клавиатуры реализовать поведение из модуля 8, подсветка нажатой клавиши, отображение
+  символа итд.
 */
 
 const lang = {
   en: "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 };
 
-console.log(lang);
-const arr = lang.en;
-lang.en = new Array(
-  lang.en.slice(0, 12).split(""),
-  lang.en.slice(12, 23).split(""),
-  lang.en.slice(23, 33).split("")
-);
+const keyboard = {
+  row: [lang.en.slice(0, 12).split(""), lang.en.slice(12, 23).split(""), lang.en.slice(23, 33).split("")],
+  notes: ["do", "re", "mi", "fa", "sol", "la", "si"]
+}
+console.log(keyboard);
 
 const html = document.querySelector('#testLodash').textContent.trim();
 const output = document.querySelector('#output');
 const compiled = _.template(html);
-const result = compiled(lang);
+const result = compiled(keyboard);
 output.innerHTML = result; // помещаем результат на html страницу;
 
-
-const buttons = Array.from(document.querySelectorAll("button"));
-const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ ".split(""); // добавил "пробел", что бы правильно срабатывал event.key;
-const pressed = document.querySelector(".pressed");
-
-const onPush = event => {
-  if (keys.includes(event.key)) {
-    let arg; //переменная для стрелочной ф-и.
-    let currentButton = buttons.find((arg) => arg.innerHTML === event.key || arg.innerHTML === 'space'); //ищем в массиве buttons кнопку, которую мы нажали;
-    currentButton.classList.add('keyboard__btn--active'); // присваиваем класс с подсветкой;
-    setTimeout(() => currentButton.classList.remove('keyboard__btn--active'), 200); //убираем класс через 200мс;
-    pressed.textContent = event.key; // помещаем клавишу, которую мы нажали в "pressed";
-  }
+const playSound = note => {
+  const audio = document.querySelector(`audio[data-note=${note}]`);
+  audio.currentTime = 0;
+  audio.play();
 };
 
-window.addEventListener('keydown', onPush);
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ ".split("");
+const pressed = document.querySelector(".pressed");
+
+window.addEventListener("keydown", function callback(e) {
+  if (keys.includes(e.key)) {
+    let currentButton = buttons.find((a) => a.innerHTML === e.key || a.innerHTML === "space");  //ищем в массиве buttons кнопку, которую мы нажали;
+    let note = currentButton.classList.add('keyboard__btn--active');  // присваиваем класс с подсветкой;
+    setTimeout(() => currentButton.classList.remove('keyboard__btn--active'), 200);
+    pressed.textContent = `"${currentButton.textContent}"`; // помещаем клавишу, которую мы нажали в "pressed";
+
+    let soundCheckbox = document.getElementById('slideThree');
+    if (soundCheckbox.checked) {
+      playSound(currentButton.dataset.note);
+    }
+  }
+}, true);
