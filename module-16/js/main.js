fbea3bf3f37815fe144b1e0e2fb5259375995a1e
Pavlo Bezsmertnyy <32062710+oneofblaze@users.noreply.github.com>
2018-04-17 14:20:30 +0300

    temp update main.js
    Module-16, temp update main.js

--- module-16/js/main.js
+++ module-16/js/main.js
@@ -1,33 +1,22 @@
-/*
-1. Создать две кнопки в HTML: start и stop.
-2. Реализовать функционал таймера отсчета старта печати через функцию-конструктор со свойсвами 
-startTime, stopTime и interval. Добавить в prototype методы start и stop.
-3. При нажатии на кнопку start, функция сохраняет момент нажатия в свойство startTime.
-4. При нажатии на кнопку stop, функция сохраняет значение текущего момента времени в stopTime 
-и записывает разницу между startTime и stopTime в interval.
-5. При нажатии на stop, значение interval выводится в консоль.*/
+/*Описание задачи в index.html*/
 
-const startButton = document.querySelector("#start");
-const stopButton = document.querySelector("#stop");
-const timerResult = document.querySelector("#timer_result");
-
-function Timer() {}
-
-Timer.prototype.startTimer = function() {
-    this.startTime = Date.now();
+class Shape {
+	constructor(color, initX, initY) {
+		this.color = color;
+		this.initX = initX;
+		this.initY = initY;
+	};
+	getColor() {
+		return this.color;
+	};
+	setColor(val) {
+		this.color = val;
+	};
+	getCoords() {
+		return [this.initX, this.initY];
+	};
+	moveTo(newX, newY) {
+		this.initX = newX;
+		this.initY = newY;
+	};
 }
-
-Timer.prototype.stopTimer = function() {
-    this.stopTime = Date.now();
-    this.interval = this.stopTime - this.startTime;
-    console.log(this.interval);
-    timerResult.textContent = this.interval + " миллисекунд или ~ " + Math.floor(this.interval / 1000) + " секунд";
-
-}
-
-let currentInterval = new Timer();
-let setStart = currentInterval.startTimer.bind(currentInterval);
-let setStop = currentInterval.stopTimer.bind(currentInterval);
-
-startButton.addEventListener("click", setStart);
-stopButton.addEventListener("click", setStop);
